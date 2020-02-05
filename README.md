7442ce2f9254c810c867f376bc1a6dcc2b20db57
Andrey Gliznetsov <andrey.gliznetsov@collibra.com>
2020-02-05 16:11:35 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# d3-vue-example
+
+Repository supporting the 'D3.js and Vue.js' blog post on Medium. Link [here](https://medium.com/@andre.devries/d3-js-and-vue-js-7a6a721eb79f)
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+https://levelup.gitconnected.com/d3-js-and-vue-js-7a6a721eb79f
+https://www.sitepoint.com/vue-d3-data-visualization-intro/
+
---
+++ src/components/Chart.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1>Circle Pack in D3</h1>
+    <h2>{{ msg }}</h2>
+    <svg :height='height' :width='width'>
+      <g transform="translate(50,50)">
+        <circle
+          v-for="c in output"
+          :key="c.id"
+          :r="c.r"
+          :cx="c.x"
+          :cy="c.y"
+          :fill="c.fill"
+          :stroke="c.stroke"
+        >
+        </circle>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "PackChart",
+  props: ["tweetData"],
+  data() {
+    return {
+      msg: "👋 from the Chart Component",
+      height: 600,
+      width: 600
+    };
+  },
+  created() {
+  },
+  methods: {
+  },
+  computed: {
+    output() {
+      var data = [];
+      var x = 0;
+      var y = 0;
+      this.tweetData.forEach(it => {
+        var r = it.retweets.length * 10;
+        data.push({r:r, x:x, y:y, fill:"#5EAFC6", stroke:"grey"});
+        x += r * 2;
+        y += r * 2;
+      });
+      //return [{r:"250", x:"250", y:"250", fill:"#5EAFC6", stroke:"grey"}]
+      return data;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
